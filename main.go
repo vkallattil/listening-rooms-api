6db1768093cdc1fe13a0f79d8fd7167ac6e8a9dd
Vaishakh <vkallattil@gmail.com>
2023-07-25 21:58:11 -0400

    hope

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"https://listening-rooms-client.onrender.com"},
+		AllowOrigins: []string{"https://listening-rooms-client.onrender.com", "http://localhost:8080"},
 		AllowHeaders: []string{"Origin", "Content-Type"},
 		AllowMethods: []string{"GET", "POST", "DELETE", "PUT"},
 	}))
